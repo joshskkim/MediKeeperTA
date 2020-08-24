76d0121bcbb33b10913609d657e37eda44f15c65
Joshua Kim <josh.sk.kim@gmail.com>
2020-08-24 10:29:34 -0700

    CSS

--- client/src/components/Items.vue
+++ client/src/components/Items.vue
@@ -1,14 +1,19 @@
 <template>
-  <table>
-    <thead>
-      <Header />
-    </thead>
-    <tbody>
-      <tr v-bind:key="item.id" v-for="item in items">
-        <SingleItem v-bind:item="item" />
-      </tr>
-    </tbody>
-  </table>
+  <div>
+    <h2>
+      Current Items
+    </h2>
+    <table>
+      <thead>
+        <Header />
+      </thead>
+      <tbody>
+        <tr v-bind:key="item.id" v-for="item in items">
+          <SingleItem v-bind:item="item" />
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
@@ -29,12 +34,18 @@ export default {
   table {
     border-collapse: collapse;
     border: 3px solid #44475C;
-    margin: 10px 10px 0 10px;
+    margin: 0 10px 0 10px;
   }
   table tbody tr:nth-child(2n) td {
     background: #D4D8F9;
   }
   table tr:hover {
     opacity: 0.5;
+  }
+</style>
+
+<style scoped>
+  h2 {
+    margin-left: 10px;
   }
 </style>
